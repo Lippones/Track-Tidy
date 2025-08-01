782490d0e14c86755cf108123e791d9540f6217f
Filipe <filipe.silva@bttis.com>
2025-08-01 14:57:46 -0300

    Add dynamic export to PlayList component

--- apps/web/src/app/playlists/page.tsx
+++ apps/web/src/app/playlists/page.tsx
@@ -6,6 +6,8 @@ import { Button } from '@workspace/ui/components/button'
 import Link from 'next/link'
 import { Suspense } from 'react'
 
+export const dynamic = 'force-dynamic'
+
 export default async function PlayList() {
   const queryClient = getQueryClient()
 
