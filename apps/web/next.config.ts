a1680ff7d7357c8ca4a18d0b0c069687f890895f
Filipe <filipe.silva@bttis.com>
2025-08-01 15:57:58 -0300

    Add PrismaPlugin to Next.js configuration

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next'
+// @ts-expect-error ?????????
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@workspace/ui'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
   images: {
     remotePatterns: [
       {
