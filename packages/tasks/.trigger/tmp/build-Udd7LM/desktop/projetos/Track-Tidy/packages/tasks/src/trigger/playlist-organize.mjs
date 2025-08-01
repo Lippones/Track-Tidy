873243365233b5bf8ccbad1799d086ef005fbcbe
Filipe <filipe.silva@bttis.com>
2025-08-01 13:46:31 -0300

    Remove obsolete index.json file and update turbo.json to remove trigger:deploy dependency from build task

--- packages/tasks/.trigger/tmp/build-Udd7LM/desktop/projetos/Track-Tidy/packages/tasks/src/trigger/playlist-organize.mjs
+++
@@ -1,9 +0,0 @@
-import {
-  playlistOrganize
-} from "../../../../../../../chunk-EJ4LUL7J.mjs";
-import "../../../../../../../chunk-PEGTRPBK.mjs";
-import "../../../../../../../chunk-5YFNUNE7.mjs";
-export {
-  playlistOrganize
-};
-//# sourceMappingURL=playlist-organize.mjs.map
